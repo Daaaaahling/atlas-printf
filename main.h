fa84eae94d0f8b7b2af3fd8c44aa1a2920880ad5
Una <una_cruz@yahoo.com>
2024-02-29 17:44:29 +0000

    added library to main.h to be able to access va_arg etc

--- main.h
+++ main.h
@@ -1,12 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+#include <stdarg.h> /* included to use va_arg, etc */
 #include <unistd.h> /* included to call write in our functions */
-
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _print_string(va_list args);
 int _print_percent(va_list args);
 
-#endif
+#endiif
