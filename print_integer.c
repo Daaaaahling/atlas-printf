f223f52813f7d4ed8f3f459e208450d284b2a4cc
daaaaahling <una_cruz@yahoo.com>
2024-03-02 18:46:19 +0000

    edit comments and formatting

--- print_integer.c
+++ print_integer.c
@@ -14,32 +14,34 @@ int print_integer(va_list args)
 
     /* Get the integer argument from va_list. */
     if (sizeof(int) == sizeof(long int))
-    {
         num = va_arg(args, long int);
-    }
+
     else
-    {
+
         num = va_arg(args, int);
-    }
 
     /* Check if the number is negative. */
     if (num < 0)
     {
-        is_negative = 1;
-        /* convert number to pisitive */
+	is_negative = 1;
+        /* convert number to positive */
         num = -num;
     }
 
     /* Get digits and store them in the buffer. */
     do
     {
-        buffer[buffer_index++] = num % 10 + '0'; /* Convert digit to character. */
+	/* Convert digit to character. */
+	buffer[buffer_index++]= num % 10 + '0';
         num /= 10;
-    } while (num > 0);
+    }
+    
+    while (num > 0);
 
-    digit_count = buffer_index; /* Count the number of digits in the integer. */
+    /* Count the number of digits in the integer. */
+    digit_count = buffer_index;
 
-    /* Add a '-' sign if the number is negative. */
+    /* Add a - sign if the number is negative. */
     if (is_negative)
     {
         buffer[buffer_index++] = '-';
@@ -53,6 +55,6 @@ int print_integer(va_list args)
         buffer[buffer_index - swap_index - 1] = temp;
     }
 
-    /* Write the number to standard output and return the character count. */
+    /* Write to standard output and return the character count. */
     return write(1, buffer, digit_count);
 }
