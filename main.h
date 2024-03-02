8f7aa1c27dc9313f49c74cd0e2975685add0d5d8
daaaaahling <una_cruz@yahoo.com>
2024-03-02 23:57:30 +0000

    typo

--- main.h
+++ main.h
@@ -6,8 +6,8 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int _print_string(va_list args);
-int _print_percent(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
 int print_integer(va_list args);
 int print_character(va_list args);
 
