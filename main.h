0fa9cc5beb475fe0a7bda9b510d45dd47c5530d7
daaaaahling <una_cruz@yahoo.com>
2024-03-02 19:16:04 +0000

    added print_integer prototype to main.h

--- main.h
+++ main.h
@@ -2,10 +2,12 @@
 #define MAIN_H
 
 #include <stdarg.h> /* included to use va_arg, etc */
-#include <unistd.h> /* included to call write in our functions */
+#include <unistd.h> /* included to call write in functions */
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _print_string(va_list args);
 int _print_percent(va_list args);
+int print_integer(va_list args);
 
 #endif
