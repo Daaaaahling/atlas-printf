c6da0a902a083ac73be88671133a997a4ee4a96e
daaaaahling <una_cruz@yahoo.com>
2024-03-03 00:04:23 +0000

    added library to main.h to be able to call strlen

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 
 #include <stdarg.h> /* included to use va_arg, etc */
 #include <unistd.h> /* included to call write in functions */
+#include <string.h> /* included to call  strlen */
 
 int _printf(const char *format, ...);
 int _putchar(char c);
